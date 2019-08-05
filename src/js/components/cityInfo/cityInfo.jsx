b132c8322392a00ed8fa7fe9a332799811360604
mdramirez071 <mdramirez071@gmail.com>
2019-08-05 14:41:38 -0700

    modified name of cityinfo file

--- src/js/components/cityInfo/cityInfo.jsx
+++ src/js/components/cityInfo/cityInfo.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class CityInformation extends Component {
+export default class CityInfo extends Component {
   constructor(props) {
     super(props);
   }
