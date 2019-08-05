ece9d22dd2bf58c99bf98fb391985a28f61c9c22
mdramirez071 <mdramirez071@gmail.com>
2019-08-05 13:22:29 -0700

    modifies packageJSON file

--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-const cityId = {
-    sandiego: 4726311, //need to retrieve ids for each cities from JSON object
-    newyork: 5128638,
-    washingtondc: 4140963,
-    london: 2643743,
-    tokyo: 1850147
-  };
+// const cityId = {
+//     sandiego: 4726311, //used to test API call using city Ids
+//     newyork: 5128638,
+//     washingtondc: 4140963,
+//     london: 2643743,
+//     tokyo: 1850147
+//   };
   app.get('/weather/:city', (req, res) => {
     city = req.params.city;
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.REACT_APP_API_KEY}&units=imperial`)
